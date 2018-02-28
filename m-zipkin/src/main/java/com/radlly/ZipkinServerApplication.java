d18273d0515fb4e92aa5e192e8163f34e0b01505
xuheng <xh@mwteck.com>
2018-02-28 09:58:25 +0800

    new project zipkin

---
+++ m-zipkin/src/main/java/com/radlly/ZipkinServerApplication.java
@@ -0,0 +1,25 @@
+package com.radlly;
+
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.context.annotation.Bean;
+
+import zipkin.storage.mysql.MySQLStorage;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+	
+	@Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(Runnable::run).build();
+    }
+}
