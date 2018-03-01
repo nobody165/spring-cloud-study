6756b99d7ac3a6f3b11517facc4eddbf7f9dfd89
xuheng <xh@mwteck.com>
2018-03-01 14:29:13 +0800

    boot cloud 版本调整

--- m-provider-user/src/main/java/com/radlly/MicProviderUserApplication.java
+++ m-provider-user/src/main/java/com/radlly/MicProviderUserApplication.java
@@ -8,11 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableCaching 
-//@EnableAsync
 @MapperScan("com.radlly.**.mapper") 
 public class MicProviderUserApplication {
 
